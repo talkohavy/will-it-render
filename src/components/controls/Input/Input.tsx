a7b08cf2a718cf4dd1a1229f25fd16a1589ce4b3
Tal Kohavy <talkohavy@gmail.com>
2024-11-30 12:32:57 +0200

    updated the Input element

--- src/components/controls/Input/Input.tsx
+++ src/components/controls/Input/Input.tsx
@@ -1,13 +1,16 @@
+import { twMerge } from 'tailwind-merge';
+
 type InputProps = {
   value: string;
   setValue: (value: any) => void;
   type?: 'text';
   placeholder?: React.HTMLInputTypeAttribute;
   disabled?: boolean;
+  className?: string;
 };
 
 export default function Input(props: InputProps) {
-  const { value, setValue, type = 'text', placeholder = '', disabled } = props;
+  const { value, setValue, type = 'text', placeholder = '', disabled, className } = props;
 
   return (
     <input
@@ -16,7 +19,7 @@ export default function Input(props: InputProps) {
       onChange={(e) => setValue(e.target.value)}
       placeholder={placeholder}
       disabled={disabled}
-      className='h-10 w-72 rounded-md border border-black p-2'
+      className={twMerge('h-10 w-72 rounded-md border border-black p-2', className)}
     />
   );
 }
